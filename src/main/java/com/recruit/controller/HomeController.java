f4a236a2f74b06573ebe8e845139e7f9c175b979
xiejinwei1986@126.com <xiejinwei1986@126.com>
2015-06-14 11:04:27 +0800

    修改文件目录结构，配置内容增加ftp文件服务器配置

--- src/main/java/com/recruit/controller/HomeController.java
+++ src/main/java/com/recruit/controller/HomeController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 首页控制器
